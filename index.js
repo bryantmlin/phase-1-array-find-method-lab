cde9085effdd1e328926364c8b6b39b15db58b16
Bryant Lin <bryantmlin@gmail.com>
2023-03-21 11:45:27 -0600

    phase 1 array find

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+function superbowlWin(array) {
+    const result = array.find(array => array.result === "W");
+    return result ? result.year : undefined;
+}
